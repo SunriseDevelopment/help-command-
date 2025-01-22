dedc3268723ba0f00640a6ba8c4ab248255ff71b
Stoolie <jkhoshaba315@gmail.com>
2025-01-21 19:08:38 -0700

    command.lua

--- command.lua
+++ command.lua
@@ -1,6 +1,6 @@
 RegisterCommand('help', function()
     notify("Discord: Discord.gg/SunsetDevelopment")
-    Wait(5000)
+    Wait(5000) -- Wait 5 sec before sending the other notification
     notify("TikTok: Tiktok.com/SunsetDevFiveM")
 end, false)
 
@@ -11,3 +11,18 @@ function notify(text)
       type = "info"
   })
 end
+
+
+-- NON OX_LIB NOTIFICATIONS CODE REMOVE THE CODE ABOVE AND UNCHECK THE CODE BELOW TO USE CHAT NOTIFICATIONS!
+
+--[[    RegisterCommand('help', function()
+  notify("Discord: Discord.gg/SunsetDevelopment")
+  Wait(5000) -- Wait 5 sec before sending the other notification
+  notify("TikTok: Tiktok.com/SunsetDevFiveM")
+end, false)
+
+function msg(text)
+  TriggerEvent("chatMessage",  "[SERVER]", {255,0,0}, text)
+end
+
+]]--
